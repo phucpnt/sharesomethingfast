6a647be4ad19338a5610e255955b491d97c2ebd7
Phuc PNT <phuc.phan@sentifi.com>
2020-08-11 11:20:46 +0700

    init

---
+++ android/app/src/main/java/com/sharesomethingfast/MainActivity.java
@@ -0,0 +1,23 @@
+package com.sharesomethingfast;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ShareSomethingFast";
+  }
+
+  // on your MainActivity Class body
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    setIntent(intent);
+  }
+
+}
